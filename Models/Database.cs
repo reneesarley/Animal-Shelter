e98f1584be015b4282899f780d6621ff9261b445
Nikki Boyd and Renee Sarley <nikki@>
2018-07-10 10:34:23 -0700

    add initial database and file structure

---
+++ Models/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using AnimalShelter;
+
+namespace AnimalShelter.Models
+{
+    public class DB
+    {
+        public static MySqlConnection Connection()
+        {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+            return conn;
+        }
+    }
+}
---
+++ Models/Shelter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDoList;
+using System;
+
+namespace AnimalShelter.Models
+{
+    public class Animal
+    {
+        private int _id;
+        private string _type;
+        private string _breed;
+        private string _name;
+        private int _age;
+        private string _gender;
+        private string _size;
+
+        public Animal(int Id = 0, string type, string breed, string name, int age, string gender, string size)
+        {
+            _id = Id;
+            _type = type;
+            _breed = breed;
+            _name = name;
+            _age = age;
+            _gender = gender;
+            _size = size;
+        }
+    }
+}
